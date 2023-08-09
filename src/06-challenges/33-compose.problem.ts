346e1926c39191f44e518c72e9bdc596b09e8956
Yulia <cany96@bk.ru>
2023-08-09 22:14:10 +0200

    feat: new stuff

--- src/06-challenges/33-compose.problem.ts
+++ src/06-challenges/33-compose.problem.ts
@@ -1,11 +1,27 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
-export const compose =
-  (...funcs: Array<(input: any) => any>) =>
-  (input: any) => {
+function compose<T1, T2>(func: (t1: T1) => T2): (t1: T1) => T2;
+function compose<T1, T2, T3>(
+  func1: (t1: T1) => T2,
+  func2: (t2: T2) => T3
+): (t1: T1) => T3;
+function compose<T1, T2, T3, T4>(
+  func1: (t1: T1) => T2,
+  func2: (t2: T2) => T3,
+  func3: (t3: T3) => T4
+): (t1: T1) => T4;
+function compose<T1, T2, T3, T4, T5>(
+  func1: (t1: T1) => T2,
+  func2: (t2: T2) => T3,
+  func3: (t3: T3) => T4,
+  func4: (t4: T4) => T5
+): (t1: T1) => T5;
+function compose(...funcs: Array<<T>(input: T) => T>) {
+  return (input: any) => {
     return funcs.reduce((acc, fn) => fn(acc), input);
   };
+}
 
 const addOne = (num: number) => {
   return num + 1;
@@ -13,20 +29,20 @@ const addOne = (num: number) => {
 
 const addTwoAndStringify = compose(addOne, addOne, String);
 
-it("Should compose multiple functions together", () => {
+it('Should compose multiple functions together', () => {
   const result = addTwoAndStringify(4);
 
-  expect(result).toEqual("6");
+  expect(result).toEqual('6');
 
   type tests = [Expect<Equal<typeof result, string>>];
 });
 
-it("Should error when the input to a function is not typed correctly", () => {
+it('Should error when the input to a function is not typed correctly', () => {
   const stringifyThenAddOne = compose(
     // addOne takes in a number - so it shouldn't be allowed after
     // a function that returns a string!
     // @ts-expect-error
     String,
-    addOne,
+    addOne
   );
 });
